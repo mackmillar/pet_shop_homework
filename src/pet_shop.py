2a251f5706e214b805783a0df8353b5966cb6771
mack millar <mackmillar@gmail.com>
2020-11-21 12:16:07 +0000

    counted the customers pets

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,7 +5,6 @@ def get_pet_shop_name(shop):
 def get_total_cash(shop):
    return shop["admin"]["total_cash"]
 
-
 def add_or_remove_cash(shop, cash):
     shop["admin"]["total_cash"] += cash
 
@@ -23,27 +22,34 @@ def get_pets_by_breed(shop, breed):
     for the_pet in shop['pets']:
         if the_pet['breed'] == breed :
             pets.append(the_pet)
-    
     return pets
 
 def find_pet_by_name(shop, name):
-    
     for pet_name in shop['pets']:
         if pet_name['name'] == name:
             return pet_name
 
 def remove_pet_by_name(shop, name):
-
     for pet_name in shop['pets']:
         if pet_name['name'] == name:
             shop['pets'].remove(pet_name)
 
 def add_pet_to_stock(shop, new_pet):
-
     shop['pets'].append(new_pet)
             
+def get_customer_cash(customer):
+    return customer["cash"]
 
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customers):
+    count = 0
+
+    for customer in customers['pets']:
+        count = len(customers['pets'])
     
+    return count
 
 
 
